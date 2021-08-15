5d794c88db8996fa37b264f7ae706b55d69c4cb6
Alejandro Romero Chavez <64499754+ajromeroch@users.noreply.github.com>
2021-08-15 15:45:42 -0400

    Update Main.js

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -26,28 +26,6 @@ export default () => {
       .catch((err) => err);
   }, []);
 
-  //Persistencia de favoritos en usuario, no funciono
-  // useEffect(() => {
-  //   console.log("entro al mount");
-  //   axios
-  //     .get("/api/users/me")
-  //     .then((res) => res.data)
-  //     .then((user) => {
-  //       console.log("user from mount", user);
-  //       return axios.get(`/api/favorites/${user.id}`);
-  //     })
-  //     .then(({ data }) => {
-  //       console.log("favs", data);
-  //       dispatch(
-  //         loginUser({
-  //           email: data[0].user.email,
-  //           status: "LOGGED_IN",
-  //           favs: [...data[0].user.favs, data[0]],
-  //         })
-  //       );
-  //     })
-  //     .catch((err) => err);
-  // }, []);
 
   return (
     <div>
@@ -59,57 +37,8 @@ export default () => {
         <Route path="/favs" component={Favorites} />
         <Route path="/" component={LandingPageContainer} />
       </Switch>
-      {/* <h1>Open Movie Data Base desde Main</h1> */}
     </div>
   );
 };
 
-//ESTO DE ABAJO NO FUNCIONO! PORQUE??
-// export default class Main extends React.Component {
-//   componentDidMount() {
-//     console.log("entro al mount");
-//     axios
-//       .get("/api/users/me")
-//       .then((res) => res.data)
-//       .then((user) => {
-//         console.log("user from mount", user);
-//         const dispatch = useDispatch();
-//         dispatch(loginUser({ email: user.email, status: "LOGGED_IN" }));
-//       })
-//       // .catch(({ response }) => {
-//       //   error(response.status, response.statusText);
-//       // });
-//       .catch((err) => err);
-//   }
 
-//   // componentDidUpdate() {
-//   //   console.log("entro al didUpdate");
-//   //   axios
-//   //     .get("/api/users/me")
-//   //     .then((res) => res.data)
-//   //     .then((user) => {
-//   //       console.log("user from mount", user);
-//   //       useDispatch(loginUser({ email: user.email, status: "LOGGED_IN" }));
-//   //     })
-//   //     // .catch(({ response }) => {
-//   //     //   error(response.status, response.statusText);
-//   //     // });
-//   //     .catch((err) => err);
-//   // }
-
-//   render() {
-//     return (
-//       <div>
-//         <Switch>
-//           <Route exact path="/movies" component={MoviesContainer} />
-//           <Route path="/movies/:movieId" component={OneMovieContainer} />
-//           <Route path="/login" component={UserLoginContainer} />
-//           <Route path="/register" component={UserCreation} />
-//           <Route path="/favs" component={Favorites} />
-//           <Route path="/" component={LandingPageContainer} />
-//         </Switch>
-//         {/* <h1>Open Movie Data Base desde Main</h1> */}
-//       </div>
-//     );
-//   }
-// }
