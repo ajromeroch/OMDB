f0f03c942ac4025f4a0c23a6362309150cdfbe66
Alejandro Romero Chavez <64499754+ajromeroch@users.noreply.github.com>
2021-08-15 15:50:39 -0400

    Update users.js

--- src/store/users.js
+++ src/store/users.js
@@ -1,30 +1,11 @@
 import { createAction, createReducer } from "@reduxjs/toolkit";
 
-//export const setMovie = createAction("setMovie")
 
 export const loginUser = createAction("loginUser");
 export const logoutUser = createAction("logoutUser");
 
-// , (user) => {
-//   //obtiene al usuario
-//   if (!user) return;
-//   return axios
-//     .post("/api/users/login", {
-//       email: user.email,
-//       password: user.password,
-//     })
-//     .then(({ data }) => data)
-//     .catch((err) => {
-
-//       console.log("el error es", err.request);
-//       return err;
-//     });
-// });
 
 export const usersReducer = createReducer([], {
-  //[setMovie]: (state, action) => action.payload,
   [loginUser]: (state, action) => action.payload,
-  //porque esto no funciona??
-  //[logoutUser]: (state, action) => (state = []),
-  //[loginUser.fulfilled]: (state, action) => action.payload,
+
 });
